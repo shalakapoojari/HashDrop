6cac92bc32903fbd2642a6c7e0066cc85c956990
shalakapoojari <shalakapoojari677@gmail.com>
2025-02-26 19:03:41 +0530

    Fixed dependencies and upgraded pip

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pip install --upgrade pip
+pip install -r requirements.txt
+pip install --no-cache-dir --upgrade pip setuptools wheel
+pip install --no-cache-dir --force-reinstall cryptography pyOpenSSL
