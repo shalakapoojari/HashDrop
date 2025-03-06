aa3e5dedacd9912f63ea36c492a12ec82323553c
shalaka <167986996+shalakapoojari@users.noreply.github.com>
2025-03-06 10:14:56 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,6 +5,10 @@ from flask import render_template
 app = create_app()
 CORS(app)
 
+@app.route('/<path:path>')
+    def catch_all(path):
+        return render_template('404.html'), 404
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
