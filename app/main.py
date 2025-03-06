404a43c8cec41e6bcef8b51deac05330db395c83
shalaka <167986996+shalakapoojari@users.noreply.github.com>
2025-03-06 09:59:51 +0530

    error handler in main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from app import create_app
 from flask_cors import CORS
+from flask import render_template
 
 app = create_app()
 CORS(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
