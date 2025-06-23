9da3ca60143f8a162d751c79f992e9a3ce76c9bb
shalaka <167986996+shalakapoojari@users.noreply.github.com>
2025-06-23 21:36:20 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,19 +2,20 @@ import os
 from dotenv import load_dotenv
 from flask_mail import Mail
 
+# Load environment variables from .env (for local dev only)
+load_dotenv()
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
-    MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://user1:shalakapoojari@hashdrop.1qnye.mongodb.net/db')
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'hashdropco@gmail.com')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'nlpc ixix vdem cqdi')
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER', 'hashdropco@gmail.com')
-    UPLOAD_FOLDER = './encrypted_files'
-    HCAPTCHA_SITE_KEY = 'a04e9902-ee6a-4716-a550-ed944a76314e'  # Replace with your site key
-    HCAPTCHA_SECRET_KEY= 'ES_6992a325e3ea4ead98228b6d2c2eb5ff'  # Replace with your secret key
+    MONGO_URI = os.environ.get('MONGO_URI')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'True') == 'True'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', './encrypted_files')
+    HCAPTCHA_SITE_KEY = os.environ.get('HCAPTCHA_SITE_KEY')
+    HCAPTCHA_SECRET_KEY = os.environ.get('HCAPTCHA_SECRET_KEY')
 
-
-# Instantiate Mail but DO NOT initialize with app here
 mail = Mail()
